759700b2686b3543661677f1654ece559781e985
Reeya Seebun <reeya_seebun@hotmail.com>
2019-10-30 11:47:23 +0400

    Add-Timer Feature added

--- BatteryMonitor/Form1.cs
+++ BatteryMonitor/Form1.cs
@@ -30,6 +30,10 @@ namespace BatteryMonitor
         {
 
         }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
